9d1cdd3a3d6fe060f1b4c0c6676680e8fcb68824
unknown <you@example.com>
2023-01-20 14:22:37 -0300

    New componenets

---
+++ Assets/Script/Navigation.js
@@ -0,0 +1,8 @@
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("mySidenav").style.height = "130px";
+    }
+    
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
